b35fccb4803c11cd8f0e11c14936e774febf75d1
Robin Chauhan <robinchauhan5000@gmail.com>
2020-05-09 21:51:25 +0530

    Calculator Final Project of Course

---
+++ app/src/main/java/com/e/calculator/logic/LogicInterface.java
@@ -0,0 +1,13 @@
+package com.e.calculator.logic;
+
+/**
+ * Defines the interface for processing user calculations.
+ */
+public interface LogicInterface {
+    /**
+     * Perform the @a operation on @a argumentOne and @a argumentTwo.
+     */
+    public void process(int argumentOne,
+                        int argumentTwo,
+                        int operation);
+}
